b730a6dd788abe6ebd6c0701bc6da7d1292aa0ed
Anastasia <akvashnina@hwdtech.com>
2019-08-28 17:41:30 +0600

    Test ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,35 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+    const headComponents = getHeadComponents()
+    headComponents.sort((x, y) => {
+      if (x.key === 'TypographyStyle') {
+        return -1
+      } else if (y.key === 'TypographyStyle') {
+        return 1
+      }
+      return 0
+    })
+    replaceHeadComponents(headComponents)
+  }
+
+  const { Helmet } = require("react-helmet")
+
+exports.onRenderBody = (
+  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+  pluginOptions
+) => {
+  const helmet = Helmet.renderStatic()
+  setHtmlAttributes(helmet.htmlAttributes.toComponent())
+  setBodyAttributes(helmet.bodyAttributes.toComponent())
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ])
+}
